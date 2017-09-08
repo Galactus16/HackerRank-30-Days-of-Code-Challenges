a8f0456b8a40eac32578b96eac5f46c2e8739a3b
Anupam Gupta <anupam.gupta@aol.com>
2017-09-08 09:28:55 -0500

    Day 17: More Exceptions
    Day 17: More Exceptions

---
+++ Day17.java
@@ -0,0 +1,13 @@
+//Write your code here
+class Calculator{
+    
+    int power(int n, int p) throws Exception{
+        int result = 0;
+        if(n < 0 || p < 0)
+            throw new Exception("n and p should be non-negative");
+        else
+            result = (int)Math.pow(n,p);
+        
+        return result;
+    }
+}
