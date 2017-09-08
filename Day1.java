c7e05ece69a45bc2c4be247916e03b60eefdd550
Anupam Gupta <anupam.gupta@aol.com>
2017-09-08 08:51:36 -0500

    Day 1: Data Types
    Day 1: Data Types

---
+++ Day1.java
@@ -0,0 +1,20 @@
+        /* Declare second integer, double, and String variables. */
+        int a;
+        double b;
+        String c;
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        a = scan.nextInt();
+        scan.nextLine();
+        b = scan.nextDouble();
+        scan.nextLine();
+        c = scan.nextLine();
+        
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(a+i);
+
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(b+d);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s+c);
